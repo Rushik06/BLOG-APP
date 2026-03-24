70cecaea434b92255a1c2a6d4042d522f92a80d4
Dumpala Rushik <dumpala.rushik@mindfiresolutions.com>
2026-03-24 12:16:00 +0530

    feat: implemented strapi admin dashboard using docker

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,12 +1,11 @@
-const API_URL =
-  process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337/api";
+const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337/api';
 
 export async function fetchAPI(path: string) {
   const res = await fetch(`${API_URL}${path}`, {
-    cache: "no-store",
+    cache: 'no-store',
   });
 
-  if (!res.ok) throw new Error("Failed to fetch");
+  if (!res.ok) throw new Error('Failed to fetch');
 
   return res.json();
 }
