96dde98118b71abfacbcc5b9ee8d690421d88505
Dumpala Rushik <dumpala.rushik@mindfiresolutions.com>
2026-03-24 12:14:43 +0530

    feat: implemented strapi setup

---
+++ lib/strapi.ts
@@ -0,0 +1,12 @@
+const API_URL =
+  process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337/api";
+
+export async function fetchAPI(path: string) {
+  const res = await fetch(`${API_URL}${path}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch");
+
+  return res.json();
+}
